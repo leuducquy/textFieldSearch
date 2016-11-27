231db8aa55c13a27e689548fa5618519b7e5e463
Quy LD <quy-ld@trente.asia>
2016-11-27 15:37:44 +0700

    Initial Commit

---
+++ TextFieldSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFieldSearch
+//
+//  Created by quy on 11/27/16.
+//  Copyright © 2016 quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextFieldSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFieldSearch
+//
+//  Created by quy on 11/27/16.
+//  Copyright © 2016 quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
